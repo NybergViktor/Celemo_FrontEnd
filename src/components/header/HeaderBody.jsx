3676f7b6a7d27397da27714924726820bef65723
Tommy Dahlin <tommy.dahlin@studerande.plushogskolan.se>
2024-04-24 16:28:13 +0200

    header started

--- src/components/header/HeaderBody.jsx
+++ src/components/header/HeaderBody.jsx
@@ -5,12 +5,27 @@ const headerBody = () => {
         <div className="headerBody">
 
         <div className="headerBody_left">
-        <img src = "src\assets\hamburger.svg" alt='hamburger'/>
-        </div>
+        <section class="dropdown-container">
+          <nav id="navbar" class="navigation" role="navigation">
+            <input id="toggle-dropdown" type="checkbox" />
+            <label class="dropdown" for="toggle-dropdown">
+              <div class="top"></div>
+              <div class="middle"></div>
+              <div class="bottom"></div>
+            </label>
 
-        <div className="headerBody_center">
-
+            <nav class="menu-dropdown">
+              <a class="link1" href="index.html">Auctions</a>
+              <a class="link1" href="index.html">About</a>
+              <a class="link1" href="index.html">Contact</a>
+              <a class="link1" href="index.html">Profile</a>
+              <a class="link1" href="#">Create Auction</a>
+              <a class="link1" href="#">Sign in / Register</a>
+            </nav>
+          </nav>
+        </section>
         </div>
+        <div className="headerBody_center"></div>
 
         <div className="headerBody_right">
         <img src = "src\assets\logo.png" class="logo"/>
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-<<<<<<< HEAD
-=======
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
->>>>>>> 2e662b0394bd5913a82a68d74f95bf1772c762e7
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
