77fa205bf251166b438acbc894cf82ac448cccb3
Johan Johnsson <johan@johnsson-net.se>
2024-04-24 14:43:46 +0200

    added footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,18 @@
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <footer className='footer'>
+            <div className='under'>
+            <p>Contact</p>
+            <p>About</p>
+            <p>Home</p>
+            </div>
+            <div className='over'>
+                <p>Sign In / Register</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
--- src/pages/startpage/Startpage.jsx
+++ src/pages/startpage/Startpage.jsx
@@ -1,5 +1,13 @@
-const Startpage = () => {
+import "./Startpage.css";
+import Footer from '../../components/footer/Footer';
 
-}
+const Startpage = () => {
+  return (
+    <>
+      {/* placera header här */}
+      <Footer></Footer>
+    </>
+  );
+};
 
 export default Startpage;
