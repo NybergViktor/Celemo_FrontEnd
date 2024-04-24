71832d2b6016a1a3141bacfb993e572321862c5d
Johan Johnsson <johan@johnsson-net.se>
2024-04-24 15:54:57 +0200

    fixed footer desktop

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,18 +1,25 @@
-import './Footer.css';
+import "./Footer.css";
 
 const Footer = () => {
-    return (
-        <footer className="footer">
-            <div className="under">
-            <a href="#">Contact</a>
-              <a href="#">About</a>
-              <a href="#">Home</a>
-            </div>
-            <div className="over">
-            <a href="#">Sign In / Register</a>
-            </div>
-        </footer>
-    )
-}
+  return (
+    <footer className="footer">
+      <div className="under">
+        <a href="#">Contact</a>
+        <a href="#">About</a>
+        <a href="#">Home</a>
+      </div>
+      <div className="over">
+        <a href="#">Sign In / Register</a>
+      </div>
+      <div className="footer-desktop">
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+        <a href="#">Home</a>
+        <a href="#">Sign In</a>
+        <a href="#">Register</a>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
