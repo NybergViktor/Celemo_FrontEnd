93c8078ded2cd34919bcbc5ee63ac20d0f6260c7
Viktor Nyberg <viktor.nyberg00@gmail.com>
2024-04-24 16:48:20 +0200

    sub-header v1 done

--- src/pages/startpage/Startpage.jsx
+++ src/pages/startpage/Startpage.jsx
@@ -1,10 +1,12 @@
 import "./Startpage.css";
 import Footer from '../../components/footer/Footer';
+import SubHeader from "../../components/sub-header/SubHeader";
 
 const Startpage = () => {
   return (
     <>
       {/* placera header här */}
+      <SubHeader></SubHeader>
       <Footer></Footer>
     </>
   );
