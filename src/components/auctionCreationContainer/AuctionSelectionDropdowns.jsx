5c62d51d88836ba2ffd84eddecc92e151b50ce91
[john gunnarsson] <[johnmessoa@gmail.com]>
2024-04-25 12:36:32 +0200

    Creating a page where to create auctions creating folders in components and in pages folder

---
+++ src/components/auctionCreationContainer/AuctionSelectionDropdowns.jsx
@@ -0,0 +1,16 @@
+import "../auctionCreationContainer/AuctionSelectionDropdowns.css";
+
+function AuctionSelectionDropdowns() {
+    return (
+        <div className="main-container">
+            <div className="selection-dropdown-container"></div>
+
+
+
+
+
+        </div>
+    );
+};
+
+export default AuctionSelectionDropdowns;
---
+++ src/pages/create-auction-page/CreateAuctionPage.jsx
@@ -0,0 +1,20 @@
+
+import AuctionSelectionDropdowns from "../../components/auctionCreationContainer/AuctionSelectionDropdowns";
+import Footer from "../../components/footer/Footer";
+import Header from "../../components/header/Header";
+
+
+
+
+function CreateAuctionPage() {
+    
+    return (
+        <div>
+            <Header />
+            <AuctionSelectionDropdowns />
+            <Footer />
+        </div>
+    );
+} ;
+
+export default CreateAuctionPage;
