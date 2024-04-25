03f4441857f90d94c112433ecb9762b07725a857
Tommy Dahlin <tommy.dahlin@studerande.plushogskolan.se>
2024-04-25 11:27:18 +0200

    stashed

--- src/pages/reviewpage/ReviewPage.jsx
+++ src/pages/reviewpage/ReviewPage.jsx
@@ -1,17 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './reviewpage/ReviewPage.css'
 import Startpage from './pages/startpage/Startpage'
 
-function App() {
+function Review() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <Startpage />
+
     </>
   )
 }
 
-export default App
+export default Review
---
+++ src/reviewMain.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Review from './pages/reviewpage/ReviewPage.jsx'
+import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Review />
+  </React.StrictMode>,
+)
