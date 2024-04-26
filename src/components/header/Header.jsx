d090cdf52a6e7a1be793d6e5d46eb490c23962bd
Tommy Dahlin <tommy.dahlin@studerande.plushogskolan.se>
2024-04-26 08:32:55 +0200

    test

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,7 +4,6 @@ import "../header/Header.css";
 const Header = () => {
   return (
     <div className="header-container">
-      
       <section className="dropdown-container">
         <nav id="navbar" className="navigation" role="navigation">
           <input id="toggle-dropdown" type="checkbox" />
@@ -13,38 +12,13 @@ const Header = () => {
             <div className="middle"></div>
             <div className="bottom"></div>
           </label>
-
           <nav className="menu-dropdown">
-<<<<<<< HEAD
-            <a className="link1" href="index.html">
-              Auctions
-            </a>
-            <a className="link1" href="#">
-              About
-            </a>
-            <a className="link1" href="#">
-              Contact
-            </a>
-            <a className="link1" href="#">
-              Profile
-            </a>
-            <a className="link1" href="#">
-              Create Auction
-            </a>
-            <a className="link1" href="#">
-              Sign in
-            </a>
-            <a className="link1" href="signup.html">
-              Register
-            </a>
-=======
             <Link to="/" >Auctions</Link>
             <Link to="" >About</Link>
             <Link to="" >Contact</Link>
             <Link to="/profile" >Profile</Link>
             <Link to="" >Create auction</Link>
             <Link to="/signup" >Sign in / Register</Link>
->>>>>>> e511cd4f9fbc62c80115d7e0257842fb1338f3ed
           </nav>
         </nav>
       </section>
