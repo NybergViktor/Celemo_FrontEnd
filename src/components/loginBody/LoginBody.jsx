19c58d69f4b1a4b93c04599f3a63e968da57bd5e
MattiYaqoob3 <matti.al-hayali@studerande.plushogskolan.se>
2024-04-29 12:13:47 +0200

    not finsh

--- src/components/loginBody/LoginBody.jsx
+++ src/components/loginBody/LoginBody.jsx
@@ -1,16 +1,16 @@
-
+import "./LoginBody.css";
 const LoginBody =() =>{
     return (
-        <div className="LoginContainer">
-             <h1>Login</h1>
+        <div className="loginStyle">
+            <div className="LoginContainer">
               <form className="loginForm">
                 <input type="text" placeholder="Username"/>
                 <input type="password" name="" id=""  placeholder="Password" />
                 <button type="">Log in</button>
                 <h3>Forgot password?</h3>
                 <h3>Create new account</h3>
-                
               </form>  
+            </div>
         </div>
     ); 
 }
