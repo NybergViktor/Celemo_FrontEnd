8835987a6895c51214ac04751e2eef7666916811
Johan Johnsson <johan@johnsson-net.se>
2024-05-01 11:55:35 +0200

    added LoginContext

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,20 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Startpage from "./pages/startpage/Startpage";
 import ProfilePage from "./pages/profilePage/ProfilePage";
 import SignupPage from "./pages/signupPage/SignupPage";
+import { useContext } from "react";
+import { LoginProvider } from "./components/context/LoginContext";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Startpage />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/signup" element={<SignupPage />} />
-      </Routes>
-    </BrowserRouter>
+    <LoginProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Startpage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/signup" element={<SignupPage />} />
+        </Routes>
+      </BrowserRouter>
+    </LoginProvider>
   );
 }
 
---
+++ src/components/context/LoginContext.jsx
@@ -0,0 +1,42 @@
+import { createContext } from "react";
+
+const LoginContext = createContext();
+
+export const LoginProvider = ({ children, username, password }) => {
+  const logIn = async () => {
+    const loginData = {
+      username: `${username}`,
+      password: `${password}`,
+    };
+
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify(loginData),
+    };
+
+    const response = await fetch(
+      "http://localhost:8080/api/auth/signin",
+      options
+    );
+
+    console.log(`Logged in as ${username}`);
+    return response;
+  };
+
+  // Temp function for button in Header.jsx
+  const handleLoginClick = async () => {
+    await logIn(username="admin45", password="admin45");
+  }
+
+  return (
+    <LoginContext.Provider value={{ logIn, handleLoginClick }}
+    >{children}
+    </LoginContext.Provider>
+  );
+};
+
+export default LoginContext;
