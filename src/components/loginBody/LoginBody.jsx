f2a3a117f84e0092f59ccc343b0d51f69b9553a1
MattiYaqoob3 <matti.al-hayali@studerande.plushogskolan.se>
2024-05-01 09:54:03 +0200

    fixing the design

--- src/components/loginBody/LoginBody.jsx
+++ src/components/loginBody/LoginBody.jsx
@@ -4,11 +4,11 @@ const LoginBody =() =>{
         <div className="loginStyle">
             <div className="LoginContainer">
               <form className="loginForm">
-                <input type="text" placeholder="Username"/>
-                <input type="password" name="" id=""  placeholder="Password" />
+                <input type="text" placeholder="Username" className="username"/>
+                <input type="password" name="" id=""  placeholder="Password" className="password"/>
                 <button type="">Log in</button>
-                <h3>Forgot password?</h3>
-                <h3>Create new account</h3>
+                <h3 className="fg">Forgot password?</h3>
+                <h3 className="ca">Create new account</h3>
               </form>  
             </div>
         </div>
