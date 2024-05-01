5c5649fdf5186c9aad4b8c385d258a4381fa252d
Viktor Nyberg <viktor.nyberg00@gmail.com>
2024-05-01 12:23:38 +0200

    started on slider component

--- src/components/BidSlider/BidSlider.jsx
+++ src/components/BidSlider/BidSlider.jsx
@@ -1,8 +1,37 @@
-const Body = () => {
-    return (
+import { useState } from "react";
 
-        <>
-        
-        </>
+const Slider = () => {
+  const [bids, setBids] = useState("");
 
-    )}
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setBids("");
+  };
+
+  return (
+    <div className="sliderContainer">
+      {/* <p>{currenPrice}</p> */}
+      <div className="bidsContainer">{/* <p>{bids length}</p> */}</div>
+
+      <form onSubmit={handleSubmit}>
+        <div className="formInput">
+          <input
+            type="text"
+            placeholder="BID"
+            onChange={(e) => setBids(e.target.value)}
+            required
+          />
+          <input
+            type="text"
+            placeholder="AUTO-BID"
+            onChange={(e) => setBids(e.target.value)}
+          />
+          <button type="submit">Place Bid</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Slider;
