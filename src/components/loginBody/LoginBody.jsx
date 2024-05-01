6ad07ef440ef6e6aec04a8f65356ae669d65b377
MattiYaqoob3 <matti.al-hayali@studerande.plushogskolan.se>
2024-05-01 11:03:08 +0200

    fixning the login style

--- src/components/loginBody/LoginBody.jsx
+++ src/components/loginBody/LoginBody.jsx
@@ -1,17 +1,16 @@
 import "./LoginBody.css";
 const LoginBody =() =>{
     return (
-        <div className="loginStyle">
-            <div className="LoginContainer">
-              <form className="loginForm">
+      <div className="LoginContainer">
+             <form className="loginForm">
                 <input type="text" placeholder="Username" className="username"/>
                 <input type="password" name="" id=""  placeholder="Password" className="password"/>
-                <button type="">Log in</button>
+                <button type="" className="button">Log in</button>
                 <h3 className="fg">Forgot password?</h3>
                 <h3 className="ca">Create new account</h3>
               </form>  
-            </div>
-        </div>
+     </div>
+          
     ); 
 }
 
