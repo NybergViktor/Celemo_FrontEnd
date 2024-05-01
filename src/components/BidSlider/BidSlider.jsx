f713f7f3e0464786a6307d96eca3e90736fe7dd5
Viktor Nyberg <viktor.nyberg00@gmail.com>
2024-05-01 17:55:14 +0200

    bid slider jsx and css

--- src/components/BidSlider/BidSlider.jsx
+++ src/components/BidSlider/BidSlider.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import AuctionContext from "../context/AuctionContext";
+import "../BidSlider/BidStyle.css";
 
 const Slider = () => {
   const [bids, setBids] = useState("");
@@ -12,24 +13,27 @@ const Slider = () => {
 
   return (
     <div className="sliderContainer">
-      {/* {AuctionContext} */}
+      <div className="currentPrice">25000kr</div>
       
-      <div className="bidsContainer">{/* <p>{bids length}</p> */}</div>
-      <form onSubmit={handleSubmit}>
-        <div className="formInput">
+      <div className="bidsContainer">32 BIDS</div>
+      <form className="formContainer" >  
+      {/* onSubmit={handleSubmit} */}
+        
           <input
+          className="bid"
             type="text"
             placeholder="BID"
             onChange={(e) => setBids(e.target.value)}
             required
           />
           <input
+          className="auto-bid"
             type="text"
             placeholder="AUTO-BID"
             onChange={(e) => setBids(e.target.value)}
           />
           <button type="submit">Place Bid</button>
-        </div>
+        
       </form>
     </div>
   );
