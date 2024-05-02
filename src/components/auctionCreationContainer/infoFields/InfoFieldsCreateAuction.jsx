c6280cb5a734bd9ff5d14722efdb444421beea39
[john gunnarsson] <[johnmessoa@gmail.com]>
2024-05-02 17:18:11 +0200

    finsihed styling and interraction with upload.jsx

--- src/components/auctionCreationContainer/infoFields/InfoFieldsCreateAuction.jsx
+++ src/components/auctionCreationContainer/infoFields/InfoFieldsCreateAuction.jsx
@@ -3,15 +3,13 @@ import "../infoFields/InfoFieldsCreateAuction.css";
 import Icon from "./UploadIcon";
 
 const InfoFieldsCreateAuction = () => {
-  const [changeColor, setChangeColor] = useState("yellow")
+
 
 
   return (
     <div className="picture-container">
       <div className="picture">
-        
           <Icon />
-        
       </div>
     </div>
   );
--- src/components/auctionCreationContainer/infoFields/UploadIcon.jsx
+++ src/components/auctionCreationContainer/infoFields/UploadIcon.jsx
@@ -2,11 +2,20 @@ import { Upload } from "react-bootstrap-icons";
 import "../infoFields/UploadIcon.css";
 import { useState } from "react";
 
+//arrow function for icon
 const Icon = () => {
   const [changeColor, setChangeColor] = useState("grey");
 
+
+  // creating an arrow function
   const handleClick = () => {
-    setChangeColor("black");
+    //if color is grey it updates the state to color orange
+    if (changeColor === "grey") {
+      setChangeColor("orange");
+      // if color is something else then grey it upgrades the state to grey
+    } else {
+      setChangeColor("grey");
+    }
   };
 
   return (
