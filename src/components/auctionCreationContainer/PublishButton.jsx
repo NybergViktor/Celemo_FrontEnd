0df0f7cbe0b7f2249866c7befea968fa669f6798
[john gunnarsson] <[johnmessoa@gmail.com]>
2024-05-03 16:50:07 +0200

    responsive design

--- src/components/auctionCreationContainer/PublishButton.jsx
+++ src/components/auctionCreationContainer/PublishButton.jsx
@@ -3,7 +3,7 @@ import "../auctionCreationContainer/PublishButton.css";
 const PublishButton = () => {
   return (
     <div>
-      <button type="button">PUBLISH</button>
+      <button className="publish-button" type="button">PUBLISH</button>
     </div>
   );
 };
