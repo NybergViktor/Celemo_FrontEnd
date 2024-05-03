a90fa114f5d969cd73727dfaedf40bef4705a6be
[john gunnarsson] <[johnmessoa@gmail.com]>
2024-05-03 12:51:17 +0200

    finsihed with inputfields

--- src/components/auctionCreationContainer/infoFields/InputFields.jsx
+++ src/components/auctionCreationContainer/infoFields/InputFields.jsx
@@ -1,21 +1,42 @@
-import "../infoFields/InputFields.css"
-
-
+import "../infoFields/InputFields.css";
 
 const InputFields = () => {
   return (
     <>
-    <div className="title-text-bottom">
-      <label className="lable">
-        <input className="title-input" type="text" placeholder="Title" />
-      </label>
-    </div>
-    <div className="description-bottom">
-    <label className="lable">
-        <input type="text" placeholder="description"/>
-    </label>
-    </div>
+      <div className="title-text-bottom">
+        <label className="lable">
+          <input className="title-input" type="text" placeholder="Title" />
+        </label>
+      </div>
 
+      <div className="description-bottom">
+        <label className="lable">
+          <textarea
+            className="textarea"
+            name="myTextarea"
+            placeholder="description"
+            cols="15"
+            rows="8"
+            minlength="10"
+            maxlength="500"
+            required
+          ></textarea>
+        </label>
+        <div className="bid-bottom">
+          <label className="lable">
+            <input
+              className="bid-input"
+              type="text"
+              placeholder="Starting Bid"
+            />
+          </label>
+          <select className="end-time" id="endtime">
+            <option value="endtime" required hidden>
+              End Time
+            </option>
+          </select>
+        </div>
+      </div>
     </>
   );
 };
