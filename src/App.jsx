dc4e6c87b902b92fe210707826d4839fcfcae1d3
Tommy Dahlin <tommy.dahlin@studerande.plushogskolan.se>
2024-05-06 14:45:10 +0200

    fixed some problems with the merge

--- src/App.jsx
+++ src/App.jsx
@@ -10,17 +10,15 @@ import AuctionPage from "./pages/auction-page/AuctionPage";
 import { AuctionProvider } from "./components/context/AuctionContext";
 import { BidProvider } from "./components/context/BidsContext";
 import CreateAuctionPage from './pages/create-auction-page/CreateAuctionPage'
-<<<<<<< HEAD
 import { ReviewProvider } from "./components/context/ReviewContext";
-=======
->>>>>>> fc5bd7e51ae2b79ca13ffd99b474337a18d13811
+
 
 function App() {
   return (
     <LoginProvider>
       <UserProvider>
         <AuctionProvider>
-<<<<<<< HEAD
+          <BidProvider>
           <ReviewProvider>
           <BrowserRouter>
             <Routes>
@@ -29,23 +27,11 @@ function App() {
               <Route path="/signup" element={<SignupPage />} />
               <Route path="/review" element={<ReviewPage />} />
               <Route path="/auction" element={<AuctionPage />} />
-                <Route path="/create-auction" element={<CreateAuctionPage />} />
+              <Route path="/create-auction" element={<CreateAuctionPage />} />
             </Routes>
           </BrowserRouter>
           </ReviewProvider>
-=======
-          <BidProvider>
-            <BrowserRouter>
-              <Routes>
-                <Route path="/" element={<Startpage />} />
-                <Route path="/profile" element={<ProfilePage />} />
-                <Route path="/signup" element={<SignupPage />} />
-                <Route path="/review" element={<ReviewPage />} />
-                <Route path="/auction" element={<AuctionPage />} />
-              </Routes>
-            </BrowserRouter>
           </BidProvider>
->>>>>>> fc5bd7e51ae2b79ca13ffd99b474337a18d13811
         </AuctionProvider>
       </UserProvider>
     </LoginProvider>
