7de2d50e7be1bbfdec68b4226fc4c615d7f201fb
Johan Johnsson <johan@johnsson-net.se>
2024-05-06 16:34:24 +0200

    test to see if env stays

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,9 @@ import { UserProvider } from "./components/context/UserContext";
 import ReviewPage from "./pages/reviewpage/ReviewPage";
 import AuctionPage from "./pages/auction-page/AuctionPage";
 import { AuctionProvider } from "./components/context/AuctionContext";
-
 import { SearchProvider } from "./components/context/SearchContext";
-
 import { BidProvider } from "./components/context/BidsContext";
-
-import CreateAuctionPage from './pages/create-auction-page/CreateAuctionPage'
+import CreateAuctionPage from "./pages/create-auction-page/CreateAuctionPage";
 
 function App() {
   return (
@@ -21,28 +18,23 @@ function App() {
       <UserProvider>
         <SearchProvider>
           <AuctionProvider>
-    <BidProvider>
-            <BrowserRouter>
-              <Routes>
-                <Route path="/" element={<Startpage />} />
-
-                <Route path="/" element={<Startpage />} />
-
-                <Route path="/profile" element={<ProfilePage />} />
-                <Route path="/signup" element={<SignupPage />} />
-                <Route path="/review" element={<ReviewPage />} />
-                <Route path="/auction" element={<AuctionPage />} />
-              </Routes>
-            </BrowserRouter>
-
-          </BidProvider>
-        </AuctionProvider>
-</SearchProvider>
-
+            <BidProvider>
+              <BrowserRouter>
+                <Routes>
+                  <Route path="/" element={<Startpage />} />
+                  <Route path="/" element={<Startpage />} />
+                  <Route path="/profile" element={<ProfilePage />} />
+                  <Route path="/signup" element={<SignupPage />} />
+                  <Route path="/review" element={<ReviewPage />} />
+                  <Route path="/auction" element={<AuctionPage />} />
+                </Routes>
+              </BrowserRouter>
+            </BidProvider>
+          </AuctionProvider>
+        </SearchProvider>
       </UserProvider>
     </LoginProvider>
   );
-
 }
 
 export default App;
