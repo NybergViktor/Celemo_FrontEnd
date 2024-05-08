43f1679bdebbf1a79d3c756d087a8e5274d4aac6
Viktor Nyberg <viktor.nyberg00@gmail.com>
2024-05-08 12:26:30 +0200

    major dropdown remake

--- src/components/dropDown/DropDown.jsx
+++ src/components/dropDown/DropDown.jsx
@@ -1,35 +1,36 @@
-import React from 'react'
-import "../dropDown/ddStyling.css"
+import React from "react";
+import "../dropDown/ddStyling.css";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export const DropDown = () => {
+  const [isActive, setIsActive] = useState(false);
 
-    
+  const handleClick = () => {
+    setIsActive((current) => !current);
+  };
 
   return (
-
-    
-    
-
-    <section className="dropdown-container">
+    <>
+      <div className="dropdown-container">
         <nav id="navbar" className="navigation" role="navigation">
-          <input id="toggle-dropdown" type="checkbox" />
+          <input id="toggle-dropdown" type="checkbox" onClick={handleClick} />
           <label className="dropdown" htmlFor="toggle-dropdown">
             <div className="top"></div>
             <div className="middle"></div>
             <div className="bottom"></div>
           </label>
-          <nav className="menu-dropdown">
-
-            <Link to="/" >Auctions</Link>
-            <Link to="" >About</Link>
-            <Link to="" >Contact</Link>
-            <Link to="/profile" >Profile</Link>
-            <Link to="/create-auction" >Create auction</Link>
-            <Link to="/signup" >Sign in / Register</Link>
-
-          </nav>
         </nav>
-      </section>
-  )
-}
+      </div>
+
+      <div className={isActive ? "dropdown-active" : "dropdown-not-active"}>
+          <Link className="dd-link" to="/">Auctions</Link>
+          <Link className="dd-link" to="">About</Link>
+          <Link className="dd-link" to="">Contact</Link>
+          <Link className="dd-link" to="/profile">Profile</Link>
+          <Link className="dd-link" to="/create-auction">Create auction</Link>
+          <Link className="dd-link" to="/signup">Sign in / Register</Link>
+      </div>
+    </>
+  );
+};
