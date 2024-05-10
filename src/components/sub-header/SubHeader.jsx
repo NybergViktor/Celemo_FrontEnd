ddb5e4df21d2e48d62b71fc5823ce661f2facf37
Johan Johnsson <johan@johnsson-net.se>
2024-05-10 15:15:22 +0200

    ongoing

--- src/components/sub-header/SubHeader.jsx
+++ src/components/sub-header/SubHeader.jsx
@@ -1,14 +1,28 @@
+import { useState } from "react";
 import "./SubHeader.css";
 
 const SubHeader = () => {
+
+  const [isFilterActive, setIsFilterActive] = useState(false);
+  const [isSearchActive, setIsSearchActive] = useState(false);
+
+  const handleFilterClick = () => {
+    setIsFilterActive((current) => !current);
+  };
+  const handleSearchClick = () => {
+    setIsSearchActive((current) => !current);
+  };
+
   return (
     <div className="con">
       <section className="sub-header-container">
-        <button className="filter-container">
+        <button className="filter-container" onClick={handleFilterClick}>
           <img src="src/components/sub-header/filter.svg" alt="filter-icon" />
           Filter
         </button>
-        <button className="search-button">Search</button>
+        <button className="search-button" onClick={handleSearchClick}>
+          Search
+        </button>
       </section>
     </div>
   );
