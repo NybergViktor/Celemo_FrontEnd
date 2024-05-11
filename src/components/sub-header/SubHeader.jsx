94932b67f2c7e6ed39b352235bd807ba7820acd8
Johan Johnsson <johan@johnsson-net.se>
2024-05-11 18:02:58 +0200

    working redo button

--- src/components/sub-header/SubHeader.jsx
+++ src/components/sub-header/SubHeader.jsx
@@ -3,10 +3,11 @@ import "./SubHeader.css";
 import { SearchContext } from "../context/SearchContext";
 
 const SubHeader = () => {
-  const { searchValue, setSearchValue } = useContext(SearchContext); 
+  const { searchValue, setSearchValue, setPageNr } = useContext(SearchContext);
   const [isFilterActive, setIsFilterActive] = useState(false);
   const [isSearchActive, setIsSearchActive] = useState(false);
-  const [inputSearch, setInputSearch ] = useState("");
+  const [isRedoActive, setIsRedoActive] = useState(false);
+  const [inputSearch, setInputSearch] = useState("");
 
   const handleFilterButton = () => {
     setIsFilterActive((current) => !current);
@@ -14,55 +15,86 @@ const SubHeader = () => {
   const handleSearchButton = () => {
     setIsSearchActive((current) => !current);
   };
+  const handleRedoButton = () => {
+    setIsRedoActive((current) => !current);
+  };
   const handleSearch = (e) => {
     const value = e.target.value;
     setInputSearch(value);
     console.log(inputSearch);
-  }
+  };
   const handleSearchClick = () => {
     setSearchValue(inputSearch);
     setInputSearch("");
     setIsSearchActive((current) => !current);
+    setIsRedoActive((current) => !current);
+    setPageNr(0);
+  };
+  const handleRedoClick = () => {
+    setIsRedoActive((current) => !current);
+    setSearchValue("getall");
+    setPageNr(0);
   }
 
   return (
     <div className="con">
       <section className="sub-header-container">
-
-        
-          <div  className="filter-container">
+        <div className="filter-container">
           <button className="filter-button" onClick={handleFilterButton}>
             <img src="src/components/sub-header/filter.svg" alt="filter-icon" />
             Filter
           </button>
 
-          <div className={isFilterActive ? "filter-dropdown-active" : "filter-dropdown-not-active"}>
-            <select className="dd-select dd-item" >
+          <div
+            className={
+              isFilterActive
+                ? "filter-dropdown-active"
+                : "filter-dropdown-not-active"
+            }
+          >
+            <select className="dd-select dd-item">
               <option>Select category</option>
               <option>Hockey</option>
               <option>Music</option>
             </select>
-            <button id="search-btn" className="dd-item">Set</button>
+            <button id="search-btn" className="dd-item">
+              Set
+            </button>
           </div>
-          </div>
-        
-          <div  className="search-container">
+        </div>
+
+        <button className={isRedoActive ? "redo-active": "redo-not-active"} onClick={handleRedoClick}>
+          <img src="public/redo_icon.svg" width="50px" />
+        </button>
+
+        <div className="search-container">
           <button className="search-button" onClick={handleSearchButton}>
             Search
           </button>
 
-          <div className={isSearchActive ? "search-dropdown-active" : "search-dropdown-not-active"}>
-            <input id="search-field" 
-              className="dd-item" 
-              placeholder="Enter search term" 
-              value={inputSearch} 
-              onChange={handleSearch}>
-              </input>
-            <button id="search-btn" className="dd-item" onClick={handleSearchClick} >Search</button>
+          <div
+            className={
+              isSearchActive
+                ? "search-dropdown-active"
+                : "search-dropdown-not-active"
+            }
+          >
+            <input
+              id="search-field"
+              className="dd-item"
+              placeholder="Enter search term"
+              value={inputSearch}
+              onChange={handleSearch}
+            ></input>
+            <button
+              id="search-btn"
+              className="dd-item"
+              onClick={handleSearchClick}
+            >
+              Search
+            </button>
           </div>
-          </div>
-          
-
+        </div>
       </section>
     </div>
   );
