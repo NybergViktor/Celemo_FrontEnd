e3101fe43836eb2d1be72c2cd316f40061c73078
[john gunnarsson] <[johnmessoa@gmail.com]>
2024-05-15 21:55:16 +0200

    still working on it

--- src/components/context/CreateAuctionContext.jsx
+++ src/components/context/CreateAuctionContext.jsx
@@ -36,6 +36,7 @@ const CreateAuctionProvider = ({ children }) => {
         resolve(reader.result);
       };
       reader.readAsDataURL(inputData.file);
+      
     });
 
     // Modify the inputData object
