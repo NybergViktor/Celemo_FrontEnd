1482e310799ef27fd89113abb5b3f8cdd1e6ab97
Viktor Nyberg <viktor.nyberg00@gmail.com>
2024-05-16 12:34:44 +0200

    styling contact and footer

--- src/pages/contactpage/ContactPage.jsx
+++ src/pages/contactpage/ContactPage.jsx
@@ -4,26 +4,25 @@ import Header from "../../components/header/Header";
 import "./ContactPage.css";
 
 const ContactPage = () => {
-    return (
-        <>
-        <Header/>
+  return (
+    <>
+      <Header />
       <div className="contact-page">
         <div className="contact-page-box">
-        <div className="contact-page-title">
-        <h1>Contact Us</h1>
+          <div className="contact-page-title">
+            <h1>Contact Us</h1>
+          </div>
+          <div className="contact-page-content">
+            <p>
+              If you want to reach us you can do so through our email <br />
+              <br />
+              <div className="email">contact@celemo.com</div>
+            </p>
+          </div>
         </div>
-        <div className="contact-page-content">
-        <p>
-          If you want to reach us you can do so through our email <br/><br/>
-          
-          <div className="email">
-            contact@celemo.com
-            </div>
-          </p>
-          </div>
-          </div>
-          </div>
-          <Footer/>
-          </>
-    )}
-    export default ContactPage;
+      </div>
+      <Footer />
+    </>
+  );
+};
+export default ContactPage;
