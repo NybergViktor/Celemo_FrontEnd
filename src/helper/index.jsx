097f07c001cdd8bf89664361e715eebf955f18f9
Tommy Dahlin <tommy.dahlin@studerande.plushogskolan.se>
2024-05-16 14:47:06 +0200

    added to dropdown menu

--- src/helper/index.jsx
+++ src/helper/index.jsx
@@ -5,3 +5,15 @@ export const authenticate = () => {
         return false
     }
 };
+
+export const authenticateRole = () => {
+    if (!localStorage.getItem("user")) {
+        return false
+    }
+    const user = JSON.parse(localStorage.getItem("user"))
+    if (user.role[0] === "ROLE_ADMIN") {
+        return true
+    } else {
+        return false
+    }
+};
