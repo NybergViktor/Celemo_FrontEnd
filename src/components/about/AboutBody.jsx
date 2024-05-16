349811d81444ee168dd8a35f12a760750a984366
MattiYaqoob3 <matti.al-hayali@studerande.plushogskolan.se>
2024-05-16 13:06:08 +0200

    adding the style

--- src/components/about/AboutBody.jsx
+++ src/components/about/AboutBody.jsx
@@ -4,19 +4,88 @@ import "./AboutBody.css";
 function AboutBody() {
   return (
     <div className='AboutContainerPage'>
-        <div className='AboutContainer'>
-            <h1 className='h1C'>CELEMO</h1>
-            <p className='aboutCelemo'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta natus eligendi ea. Alias placeat et omnis tempore deleniti, illo necessitatibus temporibus dolorum similique facilis esse, consequatur nulla nobis nisi ducimus.</p>
-          
-            <div className='About'>
-                <div className='Matti'>Matti</div>
-                <div className='Tomme'>Tomme</div>
-                <div className='Joahn'>Joahn</div>
-                <div className='John'>John</div>
-                <div className='Viktor'>Vikto</div>
+      <div className='side-cotainer'>
+        <div className='side-content'>
+          <div className='card-wrapper'>
+            <div className='card'>
+              <div className='img-content'>
+                <span className='overlay'></span>
+                <div className='crad-imge'>
+                  <img src="src/assets/user.png" alt="" className='crad-img'/>
+                </div>
+              </div>
+              <div className='card-content'>
+                <h2 className='name'>matti</h2>
+                <p className='dexription'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus nulla vero harum error non? Recusandae, consequatur! Placeat dignissimos deserunt odit saepe iste architecto atque odio. Laudantium sapiente atque repudiandae nulla?</p>
+              </div>
             </div>
 
+          </div>
+          <div className='card-wrapper'>
+            <div className='card'>
+              <div className='img-content'>
+                <span className='overlay'></span>
+                <div className='crad-imge'>
+                  <img src="src/assets/user.png" alt="" className='crad-img'/>
+                </div>
+              </div>
+              <div className='card-content'>
+                <h2 className='name'>Johan</h2>
+                <p className='dexription'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus nulla vero harum error non? Recusandae, consequatur! Placeat dignissimos deserunt odit saepe iste architecto atque odio. Laudantium sapiente atque repudiandae nulla?</p>
+              </div>
+            </div>
+
+          </div>
+          <div className='card-wrapper'>
+            <div className='card'>
+              <div className='img-content'>
+                <span className='overlay'></span>
+                <div className='crad-imge'>
+                  <img src="src/assets/user.png" alt="" className='crad-img'/>
+                </div>
+              </div>
+              <div className='card-content'>
+                <h2 className='name'>Tomme</h2>
+                <p className='dexription'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus nulla vero harum error non? Recusandae, consequatur! Placeat dignissimos deserunt odit saepe iste architecto atque odio. Laudantium sapiente atque repudiandae nulla?</p>
+              </div>
+            </div>
+
+          </div>
+          <div className='card-wrapper'>
+            <div className='card'>
+              <div className='img-content'>
+                <span className='overlay'></span>
+                <div className='crad-imge'>
+                  <img src="src/assets/user.png" alt="" className='crad-img'/>
+                </div>
+              </div>
+              <div className='card-content'>
+                <h2 className='name'>John</h2>
+                <p className='dexription'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus nulla vero harum error non? Recusandae, consequatur! Placeat dignissimos deserunt odit saepe iste architecto atque odio. Laudantium sapiente atque repudiandae nulla?</p>
+              </div>
+            </div>
+
+          </div>
+          <div className='card-wrapper'>
+            <div className='card'>
+              <div className='img-content'>
+                <span className='overlay'></span>
+                <div className='crad-imge'>
+                  <img src="src/assets/user.png" alt="" className='crad-img'/>
+                </div>
+              </div>
+              <div className='card-content'>
+                <h2 className='name'>Viktor</h2>
+                <p className='dexription'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus nulla vero harum error non? Recusandae, consequatur! Placeat dignissimos deserunt odit saepe iste architecto atque odio. Laudantium sapiente atque repudiandae nulla?</p>
+              </div>
+            </div>
+
+          </div>
+
         </div>
+
+      </div>
+        
     </div>
   )
 }
